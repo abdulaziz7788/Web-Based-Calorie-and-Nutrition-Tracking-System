037dcff3a638b2868bae57409a69ddc0c28834de
abdullah <145499052+Aqll-AL@users.noreply.github.com>
2025-12-09 23:21:52 +0300

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,6 +1,5 @@
 CREATE TABLE User(
 		user_id int primary key auto_increment,
-		username VARCHAR(100) UNIQUE NOT NULL,
 		email varchar(100) UNIQUE NOT NULL,
 		password_hash VARCHAR(255)NOT NULL,
 		first_name VARCHAR(50)NOT NULL,
@@ -10,12 +9,11 @@ CREATE TABLE User(
 		gender ENUM('MALE','FEMALE','OTHER')NOT NULL,
 		date_of_birth date NOT NULL, 
 		CHECK(date_of_birth BETWEEN '1930-01-01' AND '2020-01-01'),
-
 		current_weight FLOAT, 
 		check(current_weight BETWEEN 30 AND 250),
 		height float, 
 		check(height BETWEEN 90 AND 220),
-		activity_level ENUM('HIGH','MODERATE','LOW') NOT NULL,
+		activity_level enum('not-very-active','lightly-active','active','Very-Active') NOT NULL,
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 			ON UPDATE CURRENT_TIMESTAMP
