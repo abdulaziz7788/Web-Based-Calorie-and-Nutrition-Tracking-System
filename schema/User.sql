4ee8cffeeb448e3b412eded5d1df9f1a986a33e6
Aziz10 <195310525+Azez8@users.noreply.github.com>
2025-12-10 18:29:18 +0300

    Added separated SQL schema files (User and User_goal)

---
+++ schema/User.sql
@@ -0,0 +1,20 @@
+CREATE TABLE User(
+    user_id int primary key auto_increment,
+    email varchar(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    CHECK (first_name NOT REGEXP '[0-9]'),
+    last_name varchar(50) NOT NULL,
+    CHECK (last_name NOT REGEXP '[0-9]'),
+    gender ENUM('MALE','FEMALE','OTHER') NOT NULL,
+    date_of_birth date NOT NULL,
+    CHECK(date_of_birth BETWEEN '1930-01-01' AND '2020-01-01'),
+    current_weight FLOAT,
+    CHECK(current_weight BETWEEN 30 AND 250),
+    height FLOAT,
+    CHECK(height BETWEEN 90 AND 220),
+    activity_level ENUM('not-very-active','lightly-active','active','Very-Active') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+        ON UPDATE CURRENT_TIMESTAMP
+);
---
+++ schema/User_goal.sql
@@ -0,0 +1,17 @@
+CREATE TABLE User_goal(
+    goal_id int auto_increment primary key,
+    goal_type ENUM('WEIGHT','CALORIES','STEPS','SLEEP','BETTER_HEALTH') NOT NULL,
+    user_id int NOT NULL,
+    target_value float CHECK(target_value > 0),
+    target_unit varchar(10),
+    logged_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+
+    CHECK(end_date >= start_date),
+    UNIQUE(user_id,goal_type,start_date),
+
+    Foreign key (user_id) references User(user_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
